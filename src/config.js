c419f49f533545f2d0580b185e0108ca374a6b9e
Michinobu Maeda <michinobumaeda@gmail.com>
2018-05-19 20:51:30 +0900

    modify setup CI.

---
+++ src/config.js
@@ -0,0 +1,27 @@
+export default {
+  production: {
+    firebase: {
+      apiKey: 'AIzaSyAhEiUCXEeh4QTdiNRi68z1e_HF-NAd7Fc',
+      authDomain: 'jsasogaku.firebaseapp.com',
+      databaseURL: 'https://jsasogaku.firebaseio.com',
+      projectId: 'jsasogaku',
+      storageBucket: 'jsasogaku.appspot.com',
+      messagingSenderId: '105409434786'
+    }
+  },
+  development: {
+    firebase: {
+      apiKey: 'AIzaSyCxkfr8E0BRPRCdU7rczxQ6pBOjR9jFORM',
+      authDomain: 'jsasogaku-dev.firebaseapp.com',
+      databaseURL: 'https://jsasogaku-dev.firebaseio.com',
+      projectId: 'jsasogaku-dev',
+      storageBucket: 'jsasogaku-dev.appspot.com',
+      messagingSenderId: '540544515625'
+    }
+  },
+  messages: {
+    loadingApp: 'Loading ...',
+    loadingData: 'Loading stored data.',
+    regAccount: 'Registering the account.'
+  }
+}
